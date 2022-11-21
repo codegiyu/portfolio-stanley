4b64c740cfc4e3f7b20e5b3b16ade0524738eccc
Edward-Precious Omegbu <eomegbu@gmail.com>
2022-11-21 15:51:44 +0100

    meta image link updated

--- pages/index.js
+++ pages/index.js
@@ -87,14 +87,14 @@ export default function Home() {
         <meta property="og:type" content="portfolio" />
         <meta property="og:title" content="CodeGiyu - React Developer" />
         <meta property="og:description" content="I'm Codegiyu, a Frontend React Developer. I build beautiful, fun and interactive things with code. Programmer portfolio website built with NextJS and Tailwind CSS." />
-        <meta property="og:image" content="https://res.cloudinary.com/diirhfihi/image/upload/c_pad,b_auto:predominant,fl_preserve_transparency/v1668984821/codegiyu_t9m1rc.jpg" />
+        <meta property="og:image" content="https://res.cloudinary.com/diirhfihi/image/upload/c_pad,b_auto:predominant,fl_preserve_transparency/v1669041238/codegiyu2_caxu3o.jpg" />
         <meta property="og:url" content="https://portfolio-codegiyu.vercel.app" />
         <meta name="twitter:card" content="portfolio" />
         <meta name="twitter:title" content="CodeGiyu - React Developer" />
         <meta name="twitter:url" content="https://portfolio-codegiyu.vercel.app" />
         <meta name="twitter:site" content="@TheLonerider20" />
         <meta name="twitter:description" content="I'm Codegiyu, a Frontend React Developer. I build beautiful, fun and interactive things with code. Programmer portfolio website built with NextJS and Tailwind CSS." />
-        <meta name="twitter:image" content="https://res.cloudinary.com/diirhfihi/image/upload/c_pad,b_auto:predominant,fl_preserve_transparency/v1668984821/codegiyu_t9m1rc.jpg" />
+        <meta name="twitter:image" content="https://res.cloudinary.com/diirhfihi/image/upload/c_pad,b_auto:predominant,fl_preserve_transparency/v1669041238/codegiyu2_caxu3o.jpg" />
         <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
         <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
         <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />

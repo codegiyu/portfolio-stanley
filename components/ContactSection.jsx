742806ca3e5093e07bc172390c3cc5b351a842fe
Jay-Fred31 <70653973+Jay-Fred31@users.noreply.github.com>
2023-02-23 22:37:39 +0100

    changed input text color

--- components/ContactSection.jsx
+++ components/ContactSection.jsx
@@ -1,4 +1,30 @@
+import { useRef, useState } from "react"
+
+const initialFormValues = {
+    name : "",
+    message: "",
+    // email:""
+}
+
 const ContactSection = () => {
+
+    // using use state method to store the values from the input
+    const [formValues, setFormValues]= useState(initialFormValues);
+    
+    const emailRef = useRef(null);
+
+    const handleInputChange = (e) => {
+        // update form values on any change
+        setFormValues((prevState) =>{
+            return {...prevState, [e.target.name]: e.target.value}
+        })
+        
+    }
+    console.log(emailRef.current?.value)
+    const handleFormSubmit = (e) =>{
+        e.preventDefault();
+        console.log(formValues, emailRef.current?.value);
+    }
     return (
         <div className="relative flex flex-col bg-transparent px-4 py-8 lg:py-24 sm:px-8 md:px-16 lg:px-[2.5rem] font-montserrat">
             <div className="mx-auto px-2 mb-[40px] w-sm md:w-md">
@@ -7,7 +33,7 @@ const ContactSection = () => {
             </div>
            
            {/* Form Container */}
-            <form className="flex flex-col space-y-6 py-2 md:px-16 md:py-10 items-start md:flex-row md:justify-between md:items-center md:space-x-24 md:space-y-0 w-full">
+            <form onSubmit={handleFormSubmit} className="flex flex-col space-y-6 py-2 md:px-16 md:py-10 items-start md:flex-row md:justify-between md:items-center md:space-x-24 md:space-y-0 w-full">
                 {/* Message Conatiner */}
                 <div className="flex flex-col items-start px-2 md:py-8 space-y-12 text-left md:space-y-26 w-[100%] md:w-[50%]">
                     <h2 className="font-extrabold text-5xl md:text-6xl">Let's Talk</h2>
@@ -18,9 +44,10 @@ const ContactSection = () => {
                 </div>
                 {/* Input container */}
                 <div className="flex flex-col space-y-6 items-center w-[100%] md:w-[50%] px-2 py-8 md:py-20">
-                    <input type="text"  id="name" name="name" class="w-[100%] px-10 py-4 rounded-xl focus:outline-none bg-grey" placeholder="Your Name" required/>
-                    <input type="email" id="email" name="email" class="w-[100%] px-10 py-4 rounded-xl focus:outline-none bg-grey" placeholder="Your Email Address" required/>
-                    <textarea className="bg-grey w-[100%] px-10 py-4 focus:outline-none rounded-xl" name="message" rows={5} id="message" placeholder="Your Message" required>
+                    <input type="text" onChange={handleInputChange} id="name" name="name" value={formValues.name} className="w-[100%] px-10 py-4 rounded-xl focus:outline-none bg-grey" placeholder="Your Name" required/>
+                    <input type="email" onChange={handleInputChange} value={formValues.email} id="email" name="email" className="w-[100%] px-10 py-4 rounded-xl focus:outline-none bg-grey" placeholder="Your Email Address" required/>
+                    <textarea onChange={handleInputChange} className="bg-grey w-[100%] px-10 py-4 focus:outline-none rounded-xl" name="message" rows={5} id="message" placeholder="Your Message" required value={formValues.message}
+                    >
                     </textarea>
                     {/* Submit Button */}
                     <div className="relative md:w-[60%]">
